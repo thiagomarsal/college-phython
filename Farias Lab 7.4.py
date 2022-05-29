61657e73a9c97392684960a289cb069bffb3c0a2
Farias, Thiago <thiagomarsal.farias@gmail.com>
2022-05-28 21:40:11 -0600

    Adding Program Lunch.

---
+++ Farias Lab 7.4.py
@@ -0,0 +1,22 @@
+# IT 280 – Lab #14: Program Launch Instructions
+import subprocess
+import os
+
+
+def askForPath(text):
+    isExist = False
+    image = ''
+    while isExist is False:
+        image = input(text)
+        isExist = os.path.exists(image)
+    return image
+
+
+def main():
+    imagePath = askForPath('Please, inform the absolute path for your image: ')
+    subprocess.Popen(['C:\\Windows\\System32\\mspaint.exe', imagePath], bufsize=0)
+
+    audioPath = askForPath('Please, inform the absolute path for your audio: ')
+    subprocess.Popen(['C:\\Program Files (x86)\\VideoLAN\\\VLC\\vlc.exe', audioPath], bufsize=0)
+
+main()
